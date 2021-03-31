8ee367aa56de8b356b52a1ef14283fe57932c204
KyleM21 <44535421+KyleM21@users.noreply.github.com>
2021-03-31 17:28:20 -0400

    Upgraded Bills.js
    - working on the same thing as before

--- js/bills.js
+++ js/bills.js
@@ -4,6 +4,7 @@ Bills.js page
 
 //function done by Kerry O'Neill
 function bill() {
+	alert("called")
 	var arr = $('form[name="cForm"]').serializeArray();
 
 	//input variables for name and bill
@@ -18,7 +19,12 @@ function bill() {
 	var avg_elec = 106.94;
 	var avg_phone = 114;
 
+
+	
+	alert(result)
+	// Kerry - I got rid of the if, in favor of a switch
 	//nested if statement for electric bill
+	/*
 	if(result = "electric"){
 	alert("MEOW")
 	}
@@ -26,24 +32,32 @@ function bill() {
 	if(result = "phone"){
 	alert("phone")
 	}
+	*/
 
 	switch(result){
 		case "electric":
-		alert("MEOW1")
+			alert("Electrical!")
 			break;
 		case "phone":
+			alert("Phone!")
 			break;
 		case "car":
+			alert("Car!")
 			break;
 		case "food":
+			alert("Food!")
 			break;
 		case "house":
+			alert("House!")
 			break;
 		case "debt":
+			alert("Debt!")
 			break;
 		case "school":
+			alert("School!")
 			break;
 		case "subs":
+			alert("Subscriptions!")
 			break;
 	}
 	
