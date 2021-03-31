0da95cea76dfd9c9e53beff4fbc72c43d659e5c3
KyleM21 <44535421+KyleM21@users.noreply.github.com>
2021-03-31 17:16:55 -0400

    Added Switch Statement
    - changed the way the result is grabbed by giving it the .value function instead of the previous method.

--- js/bills.js
+++ js/bills.js
@@ -4,27 +4,48 @@ Bills.js page
 
 //function done by Kerry O'Neill
 function bill() {
-  var arr = $('form[name="cForm"]').serializeArray();
+	var arr = $('form[name="cForm"]').serializeArray();
 
-  //input variables for name and bill
-  var name = arr[0].value;
-  var bill = arr[1].value;
+	//input variables for name and bill
+	var name = arr[0].value;
+	var bill = arr[1].value;
 
-  //input variables for Bill type drop down
-  var e = document.getElementById("btype");
-  var result = e.options[e.selectedIndex].text;
+	//input variables for Bill type drop down
+	var e = document.getElementById("btype");
+	var result = e.value;
 
-  //variable for MA state average of bills
-  var avg_elec = 106.94;
-  var avg_phone = 114;
+	//variable for MA state average of bills
+	var avg_elec = 106.94;
+	var avg_phone = 114;
 
-  //nested if statement for electric bill
-if(result = "electric"){
-  alert("MEOW")
-}
+	//nested if statement for electric bill
+	if(result = "electric"){
+	alert("MEOW")
+	}
 
-if(result = "electric"){
-  alert("phone")
-}
+	if(result = "phone"){
+	alert("phone")
+	}
+
+	switch(result){
+		case "electric":
+		alert("MEOW1")
+			break;
+		case "phone":
+			break;
+		case "car":
+			break;
+		case "food":
+			break;
+		case "house":
+			break;
+		case "debt":
+			break;
+		case "school":
+			break;
+		case "subs":
+			break;
+	}
+	
 
 }
